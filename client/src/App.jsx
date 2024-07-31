b593692168fc5a7252f67a63758d227a324052b0
Joe Corrales <corralesjoe07@gmail.com>
2024-07-31 14:43:43 -0600

    branch joe

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<p>Home Page</p>} />
-            
+
             {/* Rutas de la aplicacion */}
             <Route path="/login" element={<p>Login Page</p>} />
-            <Route path="/dashboard" element={<p>Dashboard Page</p>} />
+            {/* Hay que implementar autenticacion, que solo se redirija si el usuario esta autenticado */}
+            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="*" element={<p>Error 404: Page not found</p>} />
         </Routes>
     )
