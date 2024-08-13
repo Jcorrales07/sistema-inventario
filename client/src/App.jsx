085e4f35716d98c5d671fec4dfe9bd6099c64bd5
Joe Corrales <corralesjoe07@gmail.com>
2024-08-13 12:10:18 -0600

    revisando el routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,10 @@ import { Routes, Route } from 'react-router-dom'
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<p>Home Page</p>} />
+            <Route path="/" element={<p>Home Page (Osea la pantalla de inicio)</p>} />
             
             {/* Rutas de la aplicacion */}
             <Route path="/login" element={<p>Login Page</p>} />
-            <Route path="/dashboard" element={<p>Dashboard Page</p>} />
             <Route path="*" element={<p>Error 404: Page not found</p>} />
         </Routes>
     )
