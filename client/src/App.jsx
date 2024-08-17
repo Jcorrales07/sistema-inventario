ca14efc50990026df6bab4ae2fe1fb4388a9915a
Diazgerard <gerardourraco234@gmail.com>
2024-08-17 12:44:58 -0600

    Working antes que se me olvide

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import LoginSignup from'./Components/LoginSignup/LoginSignup'
 
 function App() {
     return (
         <Routes>
-            
+            <Route path="/" element={<LoginSignup />} />
             <Route path="*"element={<p>Error 404: Page not found</p>} />
       </Routes>
     )
