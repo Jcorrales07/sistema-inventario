9fffddbc2dc59b21701153fbb069605e5d9f1c60
Daniel Dubon <danield317@ubitec.edu>
2024-08-17 18:30:08 -0600

    Rutas de Usuario y Socios

--- server/routes/socio.routes.js
+++ server/routes/socio.routes.js
@@ -2,10 +2,10 @@ const {Router} = require('express');
 const socioController = require('../controllers/socio.controllers');
 
 const router = Router();
-router.get('/socio',socioController.socioSelectAllController);
-router.post('/socio',socioController.socioCreateController);
-router.put('/socio/:id',socioController.socioUpdateController);
-router.delete('/socio/:id',socioController.socioDeleteController);
-router.get('/socio/:id',socioController.socioSelectByIdController); 
+router.get('/',socioController.socioSelectAllController);
+router.post('/',socioController.socioCreateController);
+router.put('/:id',socioController.socioUpdateController);
+router.delete('/:id',socioController.socioDeleteController);
+router.get('/:id',socioController.socioSelectByIdController); 
 
 module.exports = router;
--- server/services/socio.services.js
+++ server/services/socio.services.js
@@ -1,21 +1,51 @@
+const Socio = require('../modelos/Socio');
 
-
-exports.socioCreateService = async () => {
-
+exports.socioCreateService = async (socioData, res) => {
+    try {
+        const socio = await Socio.create(socioData);
+        return socio;
+    } catch (error) {
+        res.status(400).json({ message: 'Error durante la creación de Socio' });
+    }
 }
 
-exports.socioUpdateService = async () => {
-
+exports.socioUpdateService = async (id, socioUpdate, res) => {
+    try {
+        const socioUpdateService = await Socio.update(socioUpdate);
+        if (socioUpdateService) {
+            const edited = await Socio.findOne(id);
+            return edited;
+        }
+    } catch (error) {
+        res.status(404).json({ message: 'Id invalido, no existe u ocrurrio algún error' })
+    }
 }
 
-exports.socioDeleteService = async () => {
 
+exports.socioDeleteService = async (id, res) => {
+    try {
+        const deleteSocio = await Socio.destroy(id);
+        return deleteSocio;
+
+    } catch (error) {
+        res.status(404).json({ message: 'Id no encontrado' });
+    }
+};
+
+exports.socioSelectAllService = async (res) => {
+    try {
+        const socios = await Socio.findAll();
+        return socios;
+    } catch (error) {
+        res.status(204).json({ message: 'No hay registros' })
+    }
 }
 
-exports.socioSelectAllService = async () => {
-
-}
-
-exports.socioSelectByIdService = async () => {
-
+exports.socioSelectByIdService = async (id, res) => {
+    try {
+        const socio = await Socio.findByPk(id);
+        return socio;
+    } catch (error) {
+        res.status(404).json({ message: 'Id no encontrado' })
+    }
 }
