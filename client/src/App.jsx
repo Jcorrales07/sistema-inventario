b70a6f6ed45c6970de9dd1d6969c43a2ac476369
elote200 <kelvin.melgar13@unitec.edu>
2024-08-17 23:10:24 -0600

    Edit file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,33 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import LoginSignup from './Components/LoginSignup/LoginSignup'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LoginSignup from "./components/LoginSignup/LoginSignup.jsx";
 import {
-    HomePage,
-    CRUDProductosPage,
-    CRUDAlmacenesPage,
-    CRUDUsuariosPage,
-    CRUDProveedoresPage,
-    CRUDReportesPage,
-    ConfiguracionPage,
-} from './pages'
+  HomePage,
+  CRUDProductosPage,
+  CRUDAlmacenesPage,
+  CRUDUsuariosPage,
+  CRUDProveedoresPage,
+  CRUDReportesPage,
+  ConfiguracionPage,
+} from "./pages";
 
 function App() {
-    return (
-        <Routes>
-            
-            {/* Estas rutas deben estar protegidas, osea, que una vez que se incia sesion, se puedan acceder */}
-            <Route path="/" element={<HomePage />} />
-            <Route path="/productos" element={<CRUDProductosPage />} />
-            <Route path="/almacenes" element={<CRUDAlmacenesPage />} />
-            <Route path="/usuarios" element={<CRUDUsuariosPage />} />
-            <Route path="/proveedores" element={<CRUDProveedoresPage />} />
-            <Route path="/reportes" element={<CRUDReportesPage />} />
-            <Route path="/configuracion" element={<ConfiguracionPage />} />
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginSignup />} />
 
-            <Route path="/login" element={<LoginSignup />} />
-            <Route path="*" element={<p>Error 404: Page not found</p>} />
-        </Routes>
-    )
+      {/* Estas rutas deben estar protegidas, osea, que una vez que se incia sesion, se puedan acceder */}
+      <Route path="/" element={<HomePage />} />
+      <Route path="/productos" element={<CRUDProductosPage />} />
+      <Route path="/almacenes" element={<CRUDAlmacenesPage />} />
+      <Route path="/usuarios" element={<CRUDUsuariosPage />} />
+      <Route path="/proveedores" element={<CRUDProveedoresPage />} />
+      <Route path="/reportes" element={<CRUDReportesPage />} />
+      <Route path="/configuracion" element={<ConfiguracionPage />} />
+
+      <Route path="*" element={<p>Error 404: Page not found</p>} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
