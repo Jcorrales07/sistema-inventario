ddf9a42e3998a0e118ccd11e318bf00f9189b063
Joe Corrales <corralesjoe07@gmail.com>
2024-08-18 01:25:35 -0600

    agregando la parte de monica y haciendo varios cambios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,36 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import LoginSignup from "./components/LoginSignup/LoginSignup.jsx";
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import LoginSignup from './components/LoginSignup/LoginSignup.jsx'
 import {
-  HomePage,
-  CRUDProductosPage,
-  CRUDAlmacenesPage,
-  CRUDUsuariosPage,
-  CRUDProveedoresPage,
-  CRUDReportesPage,
-  ConfiguracionPage,
-} from "./pages";
+    HomePage,
+    CRUDProductosPage,
+    CRUDAlmacenesPage,
+    CRUDUsuariosPage,
+    CRUDProveedoresPage,
+    CRUDReportesPage,
+    ConfiguracionPage,
+} from './pages'
+
+import { RegistroUsuarios } from './components'
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/login" element={<LoginSignup />} />
+    return (
+        <Routes>
+            <Route path="/login" element={<LoginSignup />} />
 
-      {/* Estas rutas deben estar protegidas, osea, que una vez que se incia sesion, se puedan acceder */}
-      <Route path="/" element={<HomePage />} />
-      <Route path="/productos" element={<CRUDProductosPage />} />
-      <Route path="/almacenes" element={<CRUDAlmacenesPage />} />
-      <Route path="/usuarios" element={<CRUDUsuariosPage />} />
-      <Route path="/proveedores" element={<CRUDProveedoresPage />} />
-      <Route path="/reportes" element={<CRUDReportesPage />} />
-      <Route path="/configuracion" element={<ConfiguracionPage />} />
+            {/* Estas rutas deben estar protegidas, osea, que una vez que se incia sesion, se puedan acceder */}
+            <Route path="/" element={<HomePage />} />
+            <Route path="/productos" element={<CRUDProductosPage />} />
+            <Route path="/almacenes" element={<CRUDAlmacenesPage />} />
+            <Route path="/usuarios" element={<CRUDUsuariosPage />} />
+            <Route path="/usuarios/registrar" element={<RegistroUsuarios />} />
+            <Route path="/proveedores" element={<CRUDProveedoresPage />} />
+            <Route path="/reportes" element={<CRUDReportesPage />} />
+            <Route path="/configuracion" element={<ConfiguracionPage />} />
 
-      <Route path="*" element={<p>Error 404: Page not found</p>} />
-    </Routes>
-  );
+            <Route path="*" element={<p>Error 404: Page not found</p>} />
+        </Routes>
+    )
 }
 
-export default App;
+export default App
--- client/src/pages/CRUDUsuariosPage.jsx
+++ client/src/pages/CRUDUsuariosPage.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
-import FeatureNavbar from '../components/FeatureNavbar'
-import { Container } from 'react-bootstrap'
+import { Button, Container } from 'react-bootstrap'
+import { FeatureNavbar } from '../components'
 
 function CRUDUsuariosPage() {
     return (
         <div>
             <FeatureNavbar />
-            <Container fluid>
-                <h3>Usuarios</h3>
+            <Container
+                fluid
+                className="d-flex justify-content-center align-items-center"
+                style={{ height: '100vh' }}
+            >
+                <div className="d-flex  gap-3">
+                    <Button variant="dark" size="lg" href="/usuarios/registrar">
+                        Registrar nuevo usuario
+                    </Button>
+                    <Button variant="dark" size="lg">
+                        Buscar Usuario
+                    </Button>
+                </div>
             </Container>
         </div>
     )
