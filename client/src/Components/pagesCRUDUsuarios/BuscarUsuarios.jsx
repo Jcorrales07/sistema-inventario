8a65f4c883782c0ecd1f88e55a387fe7e40f38a5
elote200 <kelvin.melgar13@unitec.edu>
2024-08-26 11:10:33 -0600

    Intentando unirlo aaaaaaaaaaa

--- client/src/Components/pagesCRUDUsuarios/BuscarUsuarios.jsx
+++ client/src/Components/pagesCRUDUsuarios/BuscarUsuarios.jsx
@@ -92,248 +92,218 @@ const BuscarUsuarios = () => {
     setCurrentPage(1);
   };
 
-    const handleFilterChange = (isActive) => {
-        setFilterByActive(isActive)
-        setCurrentPage(1)
-        setShowFilterModal(false)
-    }
-
-    const resetFilters = () => {
-        setFilterByActive(null)
-        setFilterByRole(null)
-        setSearchTerm('')
-        setCurrentPage(1)
-    }
-
-    return (
-        <div>
-            <FeatureNavbar />
-            <Container
-                fluid
-                className="d-flex flex-column align-items-center min-vh-100 p-4"
-            >
-                <Row className="w-100 mb-4">
-                    <Col md={8} lg={6} className="mx-auto">
-                        <h3 className="text-center mb-4">
-                            Usuarios en base de datos
-                        </h3>
-                        <InputGroup className="mb-3">
-                            <FormControl
-                                placeholder="Buscar usuario..."
-                                value={searchTerm}
-                                onChange={(e) => {
-                                    setSearchTerm(e.target.value)
-                                    setCurrentPage(1)
-                                }}
-                            />
-                        </InputGroup>
-                    </Col>
-                </Row>
-                <Row className="w-100 mb-4">
-                    <Col md={8} lg={6} className="mx-auto">
-                        <Table striped bordered hover variant="dark">
-                            <thead>
-                                <tr>
-                                    <th>Nombre</th>
-                                    <th>Correo</th>
-                                    <th>Usuario</th>
-                                    <th>
-                                        <Button
-                                            variant="link"
-                                            onClick={() =>
-                                                setShowRoleModal(true)
-                                            }
-                                            className="text-light p-0"
-                                            style={{
-                                                fontWeight: 'bold',
-                                                textDecoration: 'none',
-                                            }}
-                                        >
-                                            Rol
-                                        </Button>
-                                    </th>
-                                    <th>
-                                        <Button
-                                            variant="link"
-                                            onClick={() =>
-                                                setShowFilterModal(true)
-                                            }
-                                            className="text-light p-0"
-                                            style={{
-                                                fontWeight: 'bold',
-                                                textDecoration: 'none',
-                                            }}
-                                        >
-                                            Activo?
-                                        </Button>
-                                    </th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {currentUsers.length > 0 ? (
-                                    currentUsers.map((user, index) => (
-                                        <tr key={index}>
-                                            <td>{user.nombre}</td>
-                                            <td>{user.correo}</td>
-                                            <td>{user.usuario}</td>
-                                            <td>{user.rol}</td>
-                                            <td>{user.active ? 'Sí' : 'No'}</td>
-                                            <td>
-                                                <Button
-                                                    variant="outline-light"
-                                                    onClick={() =>
-                                                        handleShowEditModal(
-                                                            user
-                                                        )
-                                                    }
-                                                >
-                                                    {' ••• '}
-                                                </Button>
-                                            </td>
-                                        </tr>
-                                    ))
-                                ) : (
-                                    <tr>
-                                        <td colSpan="6" className="text-center">
-                                            No hay resultados
-                                        </td>
-                                    </tr>
-                                )}
-                            </tbody>
-                        </Table>
-                    </Col>
-                </Row>
-                {(filterByActive !== null || filterByRole !== null) && (
-                    <Row className="w-100 mb-4">
-                        <Col md={8} lg={6} className="mx-auto text-center">
-                            <Button variant="secondary" onClick={resetFilters}>
-                                Restablecer Filtros
-                            </Button>
-                        </Col>
-                    </Row>
+  return (
+    <div>
+      <FeatureNavbar />
+      <Container
+        fluid
+        className="d-flex flex-column align-items-center min-vh-100 p-4"
+      >
+        <Row className="w-100 mb-4">
+          <Col md={8} lg={6} className="mx-auto">
+            <h3 className="text-center mb-4">Usuarios en base de datos</h3>
+            <InputGroup className="mb-3">
+              <FormControl
+                placeholder="Buscar usuario..."
+                value={searchTerm}
+                onChange={(e) => {
+                  setSearchTerm(e.target.value);
+                  setCurrentPage(1);
+                }}
+              />
+            </InputGroup>
+          </Col>
+        </Row>
+        <Row className="w-100 mb-4">
+          <Col md={8} lg={6} className="mx-auto">
+            <Table striped bordered hover variant="dark">
+              <thead>
+                <tr>
+                  <th>Nombre</th>
+                  <th>Correo</th>
+                  <th>Usuario</th>
+                  <th>
+                    <Button
+                      variant="link"
+                      onClick={() => setShowRoleModal(true)}
+                      className="text-light p-0"
+                      style={{
+                        fontWeight: "bold",
+                        textDecoration: "none",
+                      }}
+                    >
+                      Rol
+                    </Button>
+                  </th>
+                  <th>
+                    <Button
+                      variant="link"
+                      onClick={() => setShowFilterModal(true)}
+                      className="text-light p-0"
+                      style={{
+                        fontWeight: "bold",
+                        textDecoration: "none",
+                      }}
+                    >
+                      Activo?
+                    </Button>
+                  </th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.length > 0 ? (
+                  users.map((user, index) => (
+                    <tr key={index}>
+                      <td>{user.name}</td>
+                      <td>{user.email}</td>
+                      <td>{user.username}</td>
+                      <td>{user.role}</td>
+                      <td>{user.active ? "Sí" : "No"}</td>
+                      <td>
+                        <Button
+                          variant="outline-light"
+                          onClick={() => handleShowEditModal(user)}
+                        >
+                          {" ••• "}
+                        </Button>
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan="6" className="text-center">
+                      No hay resultados
+                    </td>
+                  </tr>
                 )}
-                <Row className="w-100 mb-4">
-                    <Col md={8} lg={6} className="mx-auto text-center">
-                        <Pagination>
-                            <Pagination.Prev
-                                onClick={() =>
-                                    handlePageChange(currentPage - 1)
-                                }
-                                disabled={currentPage === 1}
-                            />
-                            {Array.from({ length: totalPages }, (_, i) => (
-                                <Pagination.Item
-                                    key={i + 1}
-                                    active={i + 1 === currentPage}
-                                    onClick={() => handlePageChange(i + 1)}
-                                >
-                                    {i + 1}
-                                </Pagination.Item>
-                            ))}
-                            <Pagination.Next
-                                onClick={() =>
-                                    handlePageChange(currentPage + 1)
-                                }
-                                disabled={currentPage === totalPages}
-                            />
-                        </Pagination>
-                    </Col>
-                </Row>
-            </Container>
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+        {(filterByActive !== null || filterByRole !== null) && (
+          <Row className="w-100 mb-4">
+            <Col md={8} lg={6} className="mx-auto text-center">
+              <Button variant="secondary" onClick={resetFilters}>
+                Restablecer Filtros
+              </Button>
+            </Col>
+          </Row>
+        )}
+        <Row className="w-100 mb-4">
+          <Col md={8} lg={6} className="mx-auto text-center">
+            <Pagination>
+              <Pagination.Prev
+                onClick={() => handlePageChange(currentPage - 1)}
+                disabled={currentPage === 1}
+              />
+              {Array.from({ length: totalPages }, (_, i) => (
+                <Pagination.Item
+                  key={i + 1}
+                  active={i + 1 === currentPage}
+                  onClick={() => handlePageChange(i + 1)}
+                >
+                  {i + 1}
+                </Pagination.Item>
+              ))}
+              <Pagination.Next
+                onClick={() => handlePageChange(currentPage + 1)}
+                disabled={currentPage === totalPages}
+              />
+            </Pagination>
+          </Col>
+        </Row>
+      </Container>
 
-            <Modal show={showRoleModal} onHide={() => setShowRoleModal(false)}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Filtrar por Rol</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Button
-                        variant="primary"
-                        className="w-100 mb-2"
-                        onClick={() => {
-                            setFilterByRole('Administrador')
-                            setShowRoleModal(false)
-                        }}
-                    >
-                        Administrador
-                    </Button>
-                    <Button
-                        variant="primary"
-                        className="w-100 mb-2"
-                        onClick={() => {
-                            setFilterByRole('Encargado de Entradas')
-                            setShowRoleModal(false)
-                        }}
-                    >
-                        Encargado de Entradas
-                    </Button>
-                    <Button
-                        variant="primary"
-                        className="w-100 mb-2"
-                        onClick={() => {
-                            setFilterByRole('Encargado de Salidas')
-                            setShowRoleModal(false)
-                        }}
-                    >
-                        Encargado de Salidas
-                    </Button>
-                    <Button
-                        variant="primary"
-                        className="w-100 mb-2"
-                        onClick={() => {
-                            setFilterByRole('Analista de Compras')
-                            setShowRoleModal(false)
-                        }}
-                    >
-                        Analista de Compras
-                    </Button>
-                </Modal.Body>
-            </Modal>
+      <Modal show={showRoleModal} onHide={() => setShowRoleModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Filtrar por Rol</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Button
+            variant="primary"
+            className="w-100 mb-2"
+            onClick={() => {
+              setFilterByRole("Administrador");
+              setShowRoleModal(false);
+            }}
+          >
+            Administrador
+          </Button>
+          <Button
+            variant="primary"
+            className="w-100 mb-2"
+            onClick={() => {
+              setFilterByRole("Encargado de Entradas");
+              setShowRoleModal(false);
+            }}
+          >
+            Encargado de Entradas
+          </Button>
+          <Button
+            variant="primary"
+            className="w-100 mb-2"
+            onClick={() => {
+              setFilterByRole("Encargado de Salidas");
+              setShowRoleModal(false);
+            }}
+          >
+            Encargado de Salidas
+          </Button>
+          <Button
+            variant="primary"
+            className="w-100 mb-2"
+            onClick={() => {
+              setFilterByRole("Analista de Compras");
+              setShowRoleModal(false);
+            }}
+          >
+            Analista de Compras
+          </Button>
+        </Modal.Body>
+      </Modal>
 
-            <Modal
-                show={showFilterModal}
-                onHide={() => setShowFilterModal(false)}
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title>Filtrar por Estado</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Button
-                        variant="primary"
-                        className="w-100 mb-2"
-                        onClick={() => handleFilterChange(true)}
-                    >
-                        Activos
-                    </Button>
-                    <Button
-                        variant="danger"
-                        className="w-100 mb-2"
-                        onClick={() => handleFilterChange(false)}
-                    >
-                        Inactivos
-                    </Button>
-                </Modal.Body>
-            </Modal>
+      <Modal show={showFilterModal} onHide={() => setShowFilterModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Filtrar por Estado</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Button
+            variant="primary"
+            className="w-100 mb-2"
+            onClick={() => handleFilterChange(true)}
+          >
+            Activos
+          </Button>
+          <Button
+            variant="danger"
+            className="w-100 mb-2"
+            onClick={() => handleFilterChange(false)}
+          >
+            Inactivos
+          </Button>
+        </Modal.Body>
+      </Modal>
 
-            <Modal show={showEditModal} onHide={handleCloseEditModal}>
-                <Modal.Body>
-                    ¿Desea editar al usuario "{selectedUser?.nombre}"?
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button
-                        variant="danger"
-                        onClick={handleCloseEditModal}
-                        href="/usuarios/editar"
-                    >
-                        Editar Usuario
-                    </Button>
-                    <Button variant="secondary" onClick={handleCloseEditModal}>
-                        Cancelar
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </div>
-    )
-}
+      <Modal show={showEditModal} onHide={handleCloseEditModal}>
+        <Modal.Body>
+          ¿Desea editar al usuario "{selectedUser?.nombre}"?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="danger"
+            onClick={handleCloseEditModal}
+            href="/usuarios/editar"
+          >
+            Editar Usuario
+          </Button>
+          <Button variant="secondary" onClick={handleCloseEditModal}>
+            Cancelar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
 
-export default BuscarUsuarios
+export default BuscarUsuarios;
