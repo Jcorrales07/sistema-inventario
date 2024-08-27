db4f9df7ed46df12498c4e74c1a79e06dd29f2f3
Joe Corrales <corralesjoe07@gmail.com>
2024-08-27 16:00:05 -0600

    union de api con el frontend

--- server/src/app.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import categoria_routes from '../routes/categoria_routes.js'
-const app =express()
-
-app.use(express.json())
-
-app.use(categoria_routes)
-export default app;
